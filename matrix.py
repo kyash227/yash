cdb0c3023af18710d43b98def6fbe5cdf7f8b086
Aayush-eng <61198949+Aayush-eng@users.noreply.github.com>
2020-10-02 02:23:51 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,21 +1,25 @@
-# A basic code for matrix input from user 
-  
-R = int(input("Enter the number of rows:")) 
-C = int(input("Enter the number of columns:")) 
-  
-# Initialize matrix 
-matrix = [] 
-print("Enter the entries rowwise:") 
-  
-# For user input 
-for i in range(R):          # A for loop for row entries 
-    a =[] 
-    for j in range(C):      # A for loop for column entries 
-         a.append(int(input())) 
-    matrix.append(a) 
-  
-# For printing the matrix 
-for i in range(R): 
-    for j in range(C): 
-        print(matrix[i][j], end = " ") 
-    print() 
+# Program to multiply two matrices using nested loops
+
+# 3x3 matrix
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+# 3x4 matrix
+Y = [[5,8,1,2],
+    [6,7,3,0],
+    [4,5,9,1]]
+# result is 3x4
+result = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
+
+# iterate through rows of X
+for i in range(len(X)):
+   # iterate through columns of Y
+   for j in range(len(Y[0])):
+       # iterate through rows of Y
+       for k in range(len(Y)):
+           result[i][j] += X[i][k] * Y[k][j]
+
+for r in result:
+   print(r)
